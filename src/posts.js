006bc41805100a42b302b5f761a683a255b8f589
Amadou Latyr Ngom <amlatyr@gmail.com>
2017-12-18 01:34:15 -0500

    Add Fall Milestone

--- src/posts.js
+++ src/posts.js
@@ -1,9 +1,9 @@
 let posts = [
     {
-        link: "#",
-        name: "Milestone Report",
-        date: "Coming next",
-        active: false
+        link: "https://docs.google.com/document/d/1j0JwTxy9TFI1CGxyqnowRXmpWbi_feiR2kaF9wckM3Q/edit",
+        name: "Milestone Report (Fall 2017)",
+        date: "Dec 18th, 2017",
+        active: true
     },
     {
         link: "https://docs.google.com/document/d/1nwHud9JuZbcXMT6D3eX6Z-f_vCMeB6aHlWUS4JISMF4/edit?usp=sharing",
